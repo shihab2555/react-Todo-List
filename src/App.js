9586c59a800a36ebd9c23306e6af6dfe8b3106db
shihab.hazari <shihab.hazari@randomaccessinc.com>
2020-09-01 10:16:18 -0700

    Fixing the warning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import uuid from 'uuid';
+import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
